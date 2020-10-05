877eed9f7335ae0010b7f701c3edc2b6c876db1b
Frank Mathy <frankmathy@gmail.com>
2020-10-05 21:35:29 +0200

    Added Heroku build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -16,6 +17,15 @@ mongoose
   .catch(err => console.err(err));
 
 app.use('/api/items', items);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
