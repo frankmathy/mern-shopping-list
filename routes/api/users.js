8a4ca8bb8343153af7a159151fbdd72b788acb51
Frank Mathy <frankmathy@gmail.com>
2020-10-06 00:41:53 +0200

    User registration

---
+++ routes/api/users.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
+
+router.post("/", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  User.findOne({ email: email }).then((user) => {
+    if (user) return res.status(400).json({ msg: "User already exists" });
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    // Create salt & hash
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then((user) => {
+          res.json({
+            user: {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            },
+          });
+        });
+      });
+    });
+  });
+});
+
+module.exports = router;
