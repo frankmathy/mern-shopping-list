ab8340b69bf07488284b61ac8932d980cac8a25c
Frank Mathy <frankmathy@gmail.com>
2020-10-06 15:19:59 +0200

    Redux code for login token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import AppNavBar from './components/AppNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,8 +7,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { Container } from 'reactstrap';
 import { ItemModal } from './components/ItemModal';
+import { loadUser } from './actions/authActions';
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <div className="App">
