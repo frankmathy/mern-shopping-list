c7e3d88baae24f99818a8709266ea09730cd0bfc
Frank Mathy <frankmathy@gmail.com>
2020-10-06 02:37:37 +0200

    Get User Data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
+const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
 
@@ -35,6 +36,12 @@ router.post('/', (req, res) => {
       );
     });
   });
+});
+
+router.get('/user', auth, (req, res) => {
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
 });
 
 module.exports = router;
