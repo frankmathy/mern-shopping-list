a157d931144920a9187d9a901f4816e162e0a401
Frank Mathy <frankmathy@gmail.com>
2020-10-06 02:06:50 +0200

    User creation with JSON Webtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,21 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
-const User = require("../../models/User");
+const User = require('../../models/User');
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    return res.status(400).json({ msg: "Please enter all fields" });
+    return res.status(400).json({ msg: 'Please enter all fields' });
   }
 
-  User.findOne({ email: email }).then((user) => {
-    if (user) return res.status(400).json({ msg: "User already exists" });
+  User.findOne({ email: email }).then(user => {
+    if (user) return res.status(400).json({ msg: 'User already exists' });
     const newUser = new User({
       name,
       email,
-      password,
+      password
     });
 
     // Create salt & hash
@@ -23,14 +25,23 @@ router.post("/", (req, res) => {
       bcrypt.hash(newUser.password, salt, (err, hash) => {
         if (err) throw err;
         newUser.password = hash;
-        newUser.save().then((user) => {
-          res.json({
-            user: {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-            },
-          });
+        newUser.save().then(user => {
+          jwt.sign(
+            { id: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
+                }
+              });
+            }
+          );
         });
       });
     });
--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
 
 const app = express();
 
 app.use(express.json());
 
-const db = require("./config/keys").mongoURI;
+const db = config.get('mongoURI');
 
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.err(err));
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.err(err));
 
-app.use("/api/items", require("./routes/api/items"));
-app.use("/api/users", require("./routes/api/users"));
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
 
 // Server static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
